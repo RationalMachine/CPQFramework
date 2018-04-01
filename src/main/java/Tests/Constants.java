f4995608f517cd6e48be047bcae38a15a6b36cbd
Aditya <aditya.bhuyan71@gmail.com>
2018-04-01 10:59:57 +0530

    Extent Reporting and Page Objects

---
+++ src/main/java/Tests/Constants.java
@@ -0,0 +1,112 @@
+package Tests;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class Constants{
+
+
+    public  WebDriver driver;
+
+
+    public WebDriver setup(){
+        //System.setProperty("webdriver.chrome.driver",readPropFile("chromepath"));
+        //driver = new ChromeDriver();
+    		System.setProperty("webdriver.gecko.driver", readPropFile("firefoxpath"));
+        driver = new FirefoxDriver();
+    		driver.get(readPropFile("url"));
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+
+    public void waitUntilVisible(WebDriver driver, int seconds, WebElement element){
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, seconds);
+            wait.until(ExpectedConditions.visibilityOf(element));
+            wait.until(ExpectedConditions.elementToBeClickable(element));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    
+  
+       
+
+    //method for WebDriverWait until object is clickable
+    public void waitUntilClickable(WebDriver driver, int seconds, WebElement element){
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, seconds);
+            wait.until(ExpectedConditions.elementToBeClickable(element));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    //method for scroll clickable
+    public void scrollUntilClickable(WebDriver driver, WebElement element) {
+    	try {
+    		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+    		Thread.sleep(500);
+    	}catch(Exception e) {
+    		e.printStackTrace();
+    	}
+    }
+
+    //method for WebDriverWait until object is staleless
+    public void waitTillStale(WebDriver driver, int seconds, WebElement element){
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, seconds);
+            wait.until(ExpectedConditions.stalenessOf(element));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public void takeScreenshot(WebDriver driver, String path) {
+		try {
+			TakesScreenshot scr = ((TakesScreenshot)driver);
+			File screenshotFile = scr.getScreenshotAs(OutputType.FILE);
+			File finalFile = new File(path);
+			FileUtils.copyFile(screenshotFile,finalFile);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+    //method to read property file
+    public String readPropFile(String value){
+        Properties prop = new Properties();
+        String propValue="";
+        try{
+            InputStream input = new FileInputStream("property.properties");
+            prop.load(input);
+            try{
+                propValue=prop.getProperty(value);
+            }catch (Exception e){
+                System.out.println("Check property name!!!");
+                e.printStackTrace();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return propValue;
+    }
+}
