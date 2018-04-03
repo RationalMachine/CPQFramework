07c9824c671dda7f0aedae291fe29b1bb6043558
Aditya <aditya.bhuyan71@gmail.com>
2018-04-03 12:12:08 +0530

    Move a lot of the ExtenReport methods to the Constants class.

--- src/main/java/Tests/Constants.java
+++ src/main/java/Tests/Constants.java
@@ -1,28 +1,75 @@
 package Tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.apache.commons.io.FileUtils;
+import org.testng.ITestResult;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-
 
 public class Constants{
 
 
     public  WebDriver driver;
 
+    //Begins the ExtentReport stuff
+    //private static ExtentTest test;
+    //private static ExtentReports extent;
+
+    //@BeforeSuite
+    public void startSuite(){
+        LoggingIn.extent = new ExtentReports();
+        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("/Users/himrekha/IdeaProjects/CPQFlintFramework/test-output/Extent.html");
+        LoggingIn.extent.attachReporter(htmlReporter);
+    }
+
+    //@BeforeMethod
+    public void startBeforeMethod(String methodName) {
+        LoggingIn.test = LoggingIn.extent.createTest(methodName);
+        //startBeforeMethod(String method.getName());
+    }
+
+    public void runAfterTesting(ITestResult result) throws IOException {
+        switch (result.getStatus()) {
+            case ITestResult.FAILURE:
+                LoggingIn.test.fail(result.getThrowable());
+                LoggingIn.test.fail("Screenshot below: " + LoggingIn.test.addScreenCaptureFromPath(takeScreenShot(result.getMethod().getMethodName())));
+                break;
+            case ITestResult.SKIP:
+                LoggingIn.test.skip(result.getThrowable());
+                break;
+            case ITestResult.SUCCESS:
+                LoggingIn.test.pass("Passed");
+                break;
+            default:
+                break;
+        }
+        LoggingIn.extent.flush();
+    }
+
+    public String takeScreenShot(String methodName){
+        String path = "/Users/himrekha/IdeaProjects/CPQFlintFramework/test-output/" + methodName + ".png";
+        try {
+            File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(screenshotFile, new File(path));
+        } catch (Exception e) {
+            //APPLICATION_LOGS.error("Could not write screenshot" + e);
+        }
+        return path;
+    }
+    ////////
 
     public WebDriver setup(){
         //System.setProperty("webdriver.chrome.driver",readPropFile("chromepath"));
